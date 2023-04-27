41c43344bf50edb84618fdd5a0618197bc1fa77d
waterbang <409071692@qq.com>
2023-04-28 02:03:12 +0800

    :ambulance: completed test

--- README.md
+++ README.md
@@ -2,5 +2,37 @@
 Signal transmitter at will
 
 ```rust
+   let mut signal = Signal::new();
 
+        // Add a listener
+        let cb = Arc::new(Mutex::new(MyCallback {}));
+        let listener = signal.listen(cb);
+        assert_eq!(signal.listener_set.read().unwrap().len(), 1);
+
+        // Emit a signal
+        signal.emit(MyArgs { arg1: 1, arg2: 2 });
+        thread::sleep(std::time::Duration::from_secs(1));
+
+        // Remove the listener
+        signal.off(cb);
+        assert_eq!(signal.listener_set.read().unwrap().len(), 0);
+
+        // Emit a signal (should not call any listeners)
+        signal.emit(MyArgs { arg1: 3, arg2: 4 });
+        thread::sleep(std::time::Duration::from_secs(1));
+
+        // Add another listener
+        let cb2 =  Arc::new(Mutex::new(MyCallback {}))
+        let listener2 = signal.listen(cb2);
+        assert_eq!(signal.listener_set.read().unwrap().len(), 1);
+
+        // Clear all listeners
+        signal.clear();
+        assert_eq!(signal.listener_set.read().unwrap().len(), 0);
 ```
+
+
+
+>Arc<Mutex<T>>将Mutex<T>包装在Arc中，用于多线程共享所有权的情况。Arc提供了引用计数的功能，可以让多个线程共享Mutex<T>的可变引用。在需要多个线程同时访问T时，Arc<Mutex<T>>是一个不错的选择。
+
+>Mutex<Arc<T>>将Arc<T>包装在Mutex中，用于多线程共享引用的情况。Mutex提供了互斥锁的功能，可以让多个线程安全地访问Arc<T>，但每次只能有一个线程访问T。在需要多个线程共享T的只读引用时，Mutex<Arc<T>>是一个不错的选择。
--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,90 @@
 use std::{
-    any::{Any},
+    any::Any,
+    borrow::BorrowMut,
     collections::HashSet,
-    sync::{Arc, Mutex}, hash::{Hash,Hasher},
+    hash::{Hash, Hasher},
+    sync::{Arc, Mutex, RwLock},
 };
-use lazy_static::lazy_static;
-
-pub trait Callback<Args> :Send + Sync {
+pub trait Callback<Args>: Send + Sync {
     fn call(&mut self, args: Args) -> Box<dyn Any>;
-    fn clone_box(&self) -> Box<dyn Callback<Args>>;
 }
-impl<Args, F:Send + Sync> Callback<Args> for F
+impl<Args, F: Send + Sync> Callback<Args> for F
 where
-    F: Send + Sync + 'static + Clone + FnMut(Args) -> Box<dyn Any>,
+    F: Clone + FnMut(Args) -> Box<dyn Any>,
 {
-    fn call(&mut self, args: Args) ->Box<dyn Any> {
+    fn call(&mut self, args: Args) -> Box<dyn Any> {
         self(args)
     }
-    fn clone_box(&self) -> Box<dyn Callback<Args>> {
-        Box::new(self.clone())
+}
+
+// pub trait WarpCallback<Args> : Send {
+//     fn init(&mut self,args:Args) -> SharingCallback<Args> {
+//         Arc::new(Mutex::new(args))
+//     }
+// } 
+
+type SimpleCallback = dyn Callback<()>;
+type OffListener<Args> = Arc<dyn FnMut(Arc<Mutex<dyn Callback<Args>>>) -> bool>;
+type SharingCallback<Args> =  Arc<Mutex<dyn Callback<Args>>>;
+
+#[derive(Clone)]
+pub enum SignalCtor {
+    /**
+     * 返回该值，会解除监听
+     */
+    OFF,
+    /**
+     * 返回该值，会让接下来的其它监听函数不再触发
+     */
+    BREAK,
+}
+
+pub struct Signal<Args> {
+    listener_set: RwLock<HashSet<Arc<Mutex<dyn Callback<Args>>>>>,
+}
+
+impl<Args: Clone + 'static> Signal<Args> {
+    pub fn new() -> Self {
+        return Self {
+            listener_set: RwLock::new(HashSet::new()),
+        };
+    }
+
+    pub fn listen(self, cb:SharingCallback<Args>) -> OffListener<Args> {
+        self.listener_set.write().unwrap().insert(cb.clone());
+        let listener = Arc::new(move |cb| self.off(cb));
+        listener
+    }
+
+    pub fn off(&self, cb:SharingCallback<Args>) -> bool {
+        self.listener_set.write().unwrap().remove(&cb)
+    }
+
+    pub fn emit(&mut self, args: Args) {
+        let mut cbs = self.listener_set.write().unwrap();
+        for cb in cbs.borrow_mut().iter() {
+            match cb.lock().unwrap().call(args) {
+                signal => {
+                    let signal_ctor = match signal.downcast_ref::<SignalCtor>() {
+                        Some(SignalCtor::OFF) => {
+                            let cb_ref = cb.clone();
+                            // self.off(Box::new(cb_ref));
+                            break;
+                        }
+                        Some(SignalCtor::BREAK) => break,
+                        _ => panic!("Invalid signal type!"),
+                    };
+                }
+            }
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.listener_set.write().unwrap().clear();
     }
 }
 
-impl<Args> Clone for Box<dyn Callback<Args>> {
-    fn clone(&self) -> Self {
-        self.clone_box()
-    }
-}
+pub type SimpleSignal = Signal<()>;
 
 impl<Args> PartialEq for dyn Callback<Args> {
     fn eq(&self, other: &Self) -> bool {
@@ -47,76 +106,54 @@ impl<Args> Hash for dyn Callback<Args> {
     }
 }
 
- type  SimpleCallback = dyn Callback<()>;
- type OffListener<Args> = Arc<dyn FnMut(Box<dyn Callback<Args>>) -> bool + Send + Sync>;
+#[cfg(test)]
+mod tests {
+    use std::thread;
 
-// lazy_static! {
-//     static ref CALLBACK: OffListener<()> = {
-//         Arc::new(|callback: Arc<dyn Callback<()>>| {
-//             true
-//         })
-//     };
-// }
-
-
-enum SignalCtor {
-    /**
-     * 返回该值，会解除监听
-     */
-    OFF,
-    /**
-     * 返回该值，会让接下来的其它监听函数不再触发
-     */
-    BREAK,
-}
-
-pub struct Signal<Args> {
-    listener_set: Mutex<HashSet<Box<dyn Callback<Args>>>>,
-}
-
-impl<Args: Clone> Signal<Args> {
-   pub fn listen(&mut self, cb:  Box<dyn Callback<Args>+ 'static>)  {
-        self.listener_set.lock().unwrap().insert(cb.clone());
-        // let listener = Arc::new(move |cb| {
-        //     self.listener_set.lock().unwrap().remove(&cb)
-        // });
-        // listener
+    use super::*;
+    #[derive(Clone)]
+    struct MyArgs {
+        arg1: i32,
+        arg2: i32,
     }
 
-    pub fn off(&mut self, cb:  Box<dyn Callback<Args>>) -> bool {
-        self.listener_set.lock().unwrap().remove(&cb)
-    }
+    struct MyCallback {}
 
-    pub fn emit(&mut self, args: Args) {
-        let cbs = self.listener_set.lock().unwrap().clone();
-        for   mut cb in cbs {
-                 match cb.call(args.clone()).downcast::<SignalCtor>() {
-                    Ok(sig) => match *sig {
-                        SignalCtor::OFF => {
-                           self.off(cb);
-                            break;
-                        }
-                        SignalCtor::BREAK => break,
-                    },
-                    Err(_) => {}
-            }
+    impl Callback<MyArgs> for MyCallback {
+        fn call(&mut self, args: MyArgs) -> Box<dyn Any> {
+            println!("Received signal with args: ({}, {})", args.arg1, args.arg2);
+            Box::new(SignalCtor::BREAK)
         }
     }
 
-    pub fn clear(&mut self) {
-        self.listener_set.lock().unwrap().clear();
+    #[test]
+    fn test_signal() {
+        let mut signal = Signal::new();
+
+        // Add a listener
+        let cb = Arc::new(Mutex::new(MyCallback {}));
+        let listener = signal.listen(cb);
+        assert_eq!(signal.listener_set.read().unwrap().len(), 1);
+
+        // Emit a signal
+        signal.emit(MyArgs { arg1: 1, arg2: 2 });
+        thread::sleep(std::time::Duration::from_secs(1));
+
+        // Remove the listener
+        signal.off(cb);
+        assert_eq!(signal.listener_set.read().unwrap().len(), 0);
+
+        // Emit a signal (should not call any listeners)
+        signal.emit(MyArgs { arg1: 3, arg2: 4 });
+        thread::sleep(std::time::Duration::from_secs(1));
+
+        // Add another listener
+        let cb2 =  Arc::new(Mutex::new(MyCallback {}))
+        let listener2 = signal.listen(cb2);
+        assert_eq!(signal.listener_set.read().unwrap().len(), 1);
+
+        // Clear all listeners
+        signal.clear();
+        assert_eq!(signal.listener_set.read().unwrap().len(), 0);
     }
-}
-
-
-
-
-pub type SimpleSignal = Signal<()>;
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    // #[test]
 }
